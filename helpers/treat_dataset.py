7f9df1d0ad8a0d16d6f1fdb5edac6fa41b86d839
franl08 <franeves08@gmail.com>
2023-06-21 09:42:33 +0100

    all keys from datasets + number of ocurrences of each one in each file

--- helpers/treat_dataset.py
+++ helpers/treat_dataset.py
@@ -1,23 +1,42 @@
 import os
 import json
 
-files = os.listdir('../datasets')
 
-params = {}
-
-for file in files:
-    f = open('../datasets/' + file, 'r')
+def compress():
+    f = open('../datasets/jstj_acordaos.json', 'r', encoding='utf-8')
     tmp = json.load(f)
+    final = []
     f.close()
-    x = tmp[0]
-    for y in x.keys():
-        if y not in params:
-            params[y] = 1
-        else:
-            params[y] += 1
-    tmp = None
+    for val in tmp:
+        val['Sumário'] = ''
+        val['Decisão Texto Integral'] = ''
+        final.append(val)
 
-for name in params.keys():
-    print(f"{name}: {params[name]}")
+    f = open('../datasets/jstj_acordaos_comp.json', 'w', encoding='utf-8')
+    f.write(json.dumps(final))
 
-print(params)
+
+def get_data():
+    params = {}
+    files = os.listdir('../datasets')
+    for file in files:
+        if file == 'jstj_acordaos.json':
+            continue
+        f = open('../datasets/' + file, 'r', encoding='utf-8')
+        tmp = json.load(f)
+        f.close()
+        for val in tmp:
+            for y in val.keys():
+                if y not in params:
+                    params[y] = {}
+                    params[y][file] = 1
+                elif file not in params[y]:
+                    params[y][file] = 1
+                else:
+                    params[y][file] += 1
+        tmp = None
+    f = open('result.txt', 'w', encoding='utf-8')
+    f.write(json.dumps(params))
+
+
+get_data()
