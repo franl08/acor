3c035724f1bf2e2d397a665f4e066ca3db7d674a
franl08 <franeves08@gmail.com>
2023-06-27 15:20:58 +0100

    added user structure and login logic

--- models/user.js
+++ models/user.js
@@ -2,8 +2,31 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    id: Number,
-    // TODO
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    registeredOn: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    role: {
+      type: String,
+      required: true,
+      default: "USER",
+      enum: ["USER", "ADMIN", "MODERATOR"],
+    },
   },
   {
     collection: "users",
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../controllers/user");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("user");
+/* GET user by username. */
+router.get("/:username", function (req, res, next) {
+  User.getUser(req.params.username, (err, user) => {
+    if (err) {
+      next(err);
+    } else {
+      res.send(user);
+    }
+  });
+});
+
+/* POST to add user */
+router.post("/", function (req, res, next) {
+  User.createUser(req.body)
+    .then(async ({ password, ...user }) => {
+      res.send({ token: await auth.getToken(user), user });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+/* PUT to update user by username */
+router.put("/:username", function (req, res, next) {
+  User.updateUser(req.params.username, req.body)
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+/* DELETE to delete user by username */
+router.delete("/:username", function (req, res, next) {
+  User.deleteUser(req.params.username)
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
