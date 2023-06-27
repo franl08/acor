7b3e16ebf223933ddfd82122d0d21e989174c062
franl08 <franeves08@gmail.com>
2023-06-27 15:25:55 +0100

    added login

--- routes/auth.js
+++ routes/auth.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const auth = require("../utils/auth");
 const User = require("../controllers/user");
 
-
 router.post("/", function (req, res, next) {
-  User.getByUsername(req.body.username).then(async ({password, ...user}) => {
-    if (await auth.comparePasswords(req.body.password, password)) {
-      res.send({ token: await auth.getToken(user), user });
-    } else {
-      res.status(401).send(err: "Invalid username or password");
-    }
-  }).catch((err) => {
-    next(err);
-  });
+  User.getByUsername(req.body.username)
+    .then(async ({ password, ...user }) => {
+      if (await auth.comparePasswords(req.body.password, password)) {
+        res.send({ token: await auth.getToken(user), user });
+      } else {
+        res.status(401).send({ error: "Invalid username or password" });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
-
 
 module.exports = router;
