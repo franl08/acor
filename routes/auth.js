b93921c521474e6a40d0aea9d53ae25aae0cddf3
franl08 <franeves08@gmail.com>
2023-06-27 19:14:45 +0100

    fixed login

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,21 @@ const auth = require("../utils/auth");
 const User = require("../controllers/user");
 
 router.post("/", function (req, res, next) {
+  console.log(req.body);
   User.getByUsername(req.body.username)
     .then(async ({ password, ...user }) => {
-      if (await auth.comparePasswords(req.body.password, password)) {
-        res.send({ token: await auth.getToken(user), user });
+      if (password !== undefined) {
+        if (await auth.comparePasswords(req.body.password, password)) {
+          res.send({ token: await auth.getToken(user), user });
+        } else {
+          res.status(401).send({ error: "Invalid username or password" });
+        }
       } else {
         res.status(401).send({ error: "Invalid username or password" });
       }
     })
     .catch((err) => {
+      console.log(err);
       next(err);
     });
 });
